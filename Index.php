7de27e728c87afb5eb912c84582e573c6910a7cd
MifzalAdam <18DIT24F1139@student.ptss.edu.my>
2026-01-20 09:21:25 +0800

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,36 @@
+<?php
+$nama = "ADAM MIFZAL BIN MOHD FAIZAL";
+$nomatrik = "18DIT24F1139";
+$kelas = "DIT4C (SW_G2)";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Maklumat Pelajar</title>
+
+    <style>
+        body {
+            font-family: verdana;
+        }
+        img {
+            width: 200px;
+        }
+    </style>
+
+</head>
+<body>
+    
+    <h2 class="fw-bold mb-4">Profil Diri</h2>
+
+    <img src="profile2.jpeg" alt="Gambar Pelajar">
+
+    <p>NAMA: <?= $nama ?></p>
+    <p>NO MATRIK: <?= $nomatrik ?></p>
+    <p>KELAS: <?= $kelas ?></p>
+    
+    </div>
+    </div>
+</body>
+</html>
